abb1de5c2ab654f7e5ebd44c6a83f92033a3c2fc
Naing Vannath <naingvannath269@gmail.com>
2026-08-24 21:01:24 +0700

    jj

--- auth.py
+++ auth.py
@@ -2,9 +2,14 @@ import os
 import sys
 import json
 
-x=1+2
-if x==3:
+x = 1 + 2
+if x == 3:
     print(x)
 
-def my_function( x, y ):
-    return x+y
+
+def my_function(x, y):
+    return x + y
+
+age: int = 5
+def add(a: int, b: int) -> int:
+    return a + b
